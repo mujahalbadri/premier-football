ea573a7ed01790273e419e53de0782ce7978b22e
Mujah Al Badri <mujahalbadri@gmail.com>
2020-10-15 15:15:40 +0700

    initial commit

--- js/team.js
+++ js/team.js
@@ -6,7 +6,7 @@ if (!('serviceWorker' in navigator)) {
 }
 // Register service worker
 function registerServiceWorker() {
-  return navigator.serviceWorker.register('/../service-worker.js')
+  return navigator.serviceWorker.register('service-worker.js')
     .then((registration) => {
       console.log('Registrasi service worker berhasil.');
       return registration;
